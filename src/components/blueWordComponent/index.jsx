4f9aeabb52e69d51f0e6580f7e2fc716a6d0ce38
Jade Garcia <jrenegarcia11@gmail.com>
2025-03-08 19:27:27 -0700

    simple show-hide image hosted by supabase in blueword

--- src/components/blueWordComponent/index.jsx
+++ src/components/blueWordComponent/index.jsx
@@ -1,8 +1,22 @@
 import { BlueWordContainer } from "./blueWordElements";
+import { useState } from 'react';
+
 function BlueWordComponent({word}) {
-	return (
-		<BlueWordContainer>{word}</BlueWordContainer>
-	)
+    const [showWord, setShowWord] = useState(true);
+
+    const handleClick = () => {
+        setShowWord(!showWord);
+    };
+
+    return (
+        <div onClick={handleClick}>
+            {showWord ? (
+                <BlueWordContainer>{word}</BlueWordContainer>
+            ) : (
+                <img src='https://trlauvtqyqikookhwwzs.supabase.co/storage/v1/object/public/lyricimgs//House_of_leaves.jpg' alt="House of Leaves"></img>
+            )}
+        </div>
+    )
 }
 
-export default BlueWordComponent
+export default BlueWordComponent;
